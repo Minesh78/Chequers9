1e10851939f89ba30dd117172332613fba3a6fc0
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2024-03-02 16:35:13 +0530

    Compressed the helll out of the images

--- src/Components/ImageSlideshow.jsx
+++ src/Components/ImageSlideshow.jsx
@@ -1,25 +1,12 @@
 import React, { useEffect, useState }from 'react'
 import image1 from '../assets/white.png'
 
+import { photos } from '../Data';
+
 
 function ImageSlideshow() {
 
-  const photos = [
-      "https://i.ibb.co/qJVJGhf/Mr-Mrs-Deepali-Joshi-Ambavane-Kop.webp" ,
-      "https://i.ibb.co/S7y6MJK/Mr-Amit-Chavan-01.webp" ,
-      "https://i.ibb.co/FDDrprk/Mr-Arun-Patil-Kop2.webp",
-      "https://i.ibb.co/s5DF9x2/Mr-Arun-Patil-Kop3.webp",
-      "https://i.ibb.co/dJ5KQfh/Mr-Chetan-Desai.webp" ,
-      "https://i.ibb.co/FBJVN0v/Mr-Ganesh-Sawant-Bhor-Pune.webp" ,
-      "https://i.ibb.co/dWxPg6r/Mr-L-M-Kumbhar-Kop2.webp" ,
-      "https://i.ibb.co/rt7dvDY/Mr-Rohan-Kadam-2.webp" ,
-      "https://i.ibb.co/Wy80MWn/Mr-Sanjay-Sawant-Kudal-Sindhudurg01.webp",
-      "https://i.ibb.co/4VJ9Q6h/Mr-Sarjoshi-Achara-Malvan1.webp" ,
-      "https://i.ibb.co/Trq3zyh/Mr-Vikas-Mane-Islampur1.webp",
-      "https://i.ibb.co/vmVyd2b/Mr-Vikas-Mane-Islampur2.webp",
-      "https://i.ibb.co/k07mYjJ/Ray-Associates-Kop.webp" ,
-      "https://i.ibb.co/fFCfPsv/Shree-Mouni-Maharaj-Math-Patgaon1.webp" 
-   ];
+
   
   const[currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
 
---
+++ src/Data.js
@@ -0,0 +1,18 @@
+const photos = [
+    "https://i.ibb.co/6mSHhwL/Mr-Arun-Patil-Kop3.webp" ,
+    "https://i.ibb.co/ZNBKt4m/Mr-Ganesh-Sawant-Bhor-Pune.webp",
+    "https://i.ibb.co/dc6MrXJ/Mr-Sarjoshi-Achara-Malvan1.webp" ,
+    "https://i.ibb.co/hLhbgwY/Ray-Associates-Kop.webp" ,
+    "https://i.ibb.co/KDK1f18/Mr-Chetan-Desai.webp" ,
+    "https://i.ibb.co/dj4Gz0x/Mr-Sanjay-Sawant-Kudal-Sindhudurg01.webp" ,
+    "https://i.ibb.co/xqYK4F6/Mr-Vikas-Mane-Islampur2.webp",
+    "https://i.ibb.co/jzp7ZK0/Mr-Vikas-Mane-Islampur1.webp" ,
+    "https://i.ibb.co/pKh6K7Y/Mr-Arun-Patil-Kop2.webp",
+    "https://i.ibb.co/gFVHXKL/Mr-Mrs-Deepali-Joshi-Ambavane-Kop.webp" ,
+    "https://i.ibb.co/NNCFkNb/Mr-L-M-Kumbhar-Kop2.webp" ,
+    "https://i.ibb.co/MsBLYCc/Mr-Amit-Chavan-01.webp" ,
+    "https://i.ibb.co/t3sxR5T/Shree-Mouni-Maharaj-Math-Patgaon1.webp" ,
+    "https://i.ibb.co/Q64YHSg/Mr-Rohan-Kadam-2.webp" ,
+ ];
+
+ export {photos}

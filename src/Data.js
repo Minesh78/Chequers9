89bd0241ee42442f20a66a40174582dbac083b91
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2024-08-03 20:05:58 +0530

    update project image links

--- src/Data.js
+++ src/Data.js
@@ -47,30 +47,31 @@ const architecturalProjects = [
 		location: "Kolhapur",
 		type: "architectural",
 		imageLink:
-			"https://i.ibb.co/R3wrxmg/Mr-Mrs-Deepali-Joshi-Ambavane-Kop.webp",
+			"https://i.ibb.co/LCrwmSN/Mr-Mrs-Deepali-Joshi-Ambavane-Kop.webp",
 	},
 	{
 		id: "02",
 		title: "Project for Mr. Amit Chavan",
 		location: "-",
 		type: "interior",
-		imageLink: "https://i.ibb.co/XsZJJ2n/Mr-Amit-Chavan-01.webp",
-		otherImages: ["https://i.ibb.co/WgZ9bn4/Mr-Amit-Chavan-02.webp"],
+		imageLink: "https://i.ibb.co/Q6nZnRQ/Mr-Amit-Chavan-01.webp",
+		otherImages: ["https://i.ibb.co/9ZbQ3rH/Mr-Amit-Chavan-02.webp"],
 	},
 	{
 		id: "03",
 		title: "Project for Mr. Arun Patil",
 		location: "Kolhapur",
 		type: "architectural",
-		imageLink: "https://i.ibb.co/3Npmjmx/Mr-Arun-Patil-Kop2.webp",
-		otherImages: ["https://i.ibb.co/WnQzKHR/Mr-Arun-Patil-Kop2.webp"],
+		imageLink: "https://i.ibb.co/dr3FNhG/Mr-Arun-Patil-Kop2.webp",
+		otherImages: ["https://i.ibb.co/Bn3SkVZ/Mr-Arun-Patil-Kop1.webp",
+			"https://i.ibb.co/6Jcnc4x/Mr-Arun-Patil-Kop3.webp"],
 	},
 	{
 		id: "04",
 		title: "Project for Mr. Ganesh Sawant",
 		location: "Bhar, Pune",
 		type: "architectural",
-		imageLink: "https://i.ibb.co/s20v88r/Mr-Ganesh-Sawant-Bhor-Pune.webp",
+		imageLink: "https://i.ibb.co/ky5whLz/Mr-Ganesh-Sawant-Bhor-Pune.webp",
 		otherImages: [],
 	},
 	{
@@ -78,8 +79,8 @@ const architecturalProjects = [
 		title: "Project for Mr. L M Kumbhar",
 		location: "Kolhapur",
 		type: "architectural",
-		imageLink: "https://i.ibb.co/kQzXsbY/Mr-L-M-Kumbhar-Kop1.webp",
-		otherImages: ["https://i.ibb.co/ZJmht0s/Mr-L-M-Kumbhar-Kop2.webp"],
+		imageLink: "https://i.ibb.co/L9R60nf/Mr-L-M-Kumbhar-Kop1.webp",
+		otherImages: ["https://i.ibb.co/z4Hv72j/Mr-L-M-Kumbhar-Kop2.webp"],
 	},
 	{
 		id: "06",
@@ -87,9 +88,9 @@ const architecturalProjects = [
 		location: "kudal, Sindhudurg",
 		type: "architectural",
 		imageLink:
-			"https://i.ibb.co/yqZNpFJ/Mr-Sanjay-Sawant-Kudal-Sindhudurg01.webp",
+			"https://i.ibb.co/0f14VvD/Mr-Sanjay-Sawant-Kudal-Sindhudurg01.webp",
 		otherImages: [
-			"https://i.ibb.co/0DVcpNP/Mr-Sanjay-Sawant-Kudal-Sindhudurg2.webp",
+			"https://i.ibb.co/XWmMnj9/Mr-Sanjay-Sawant-Kudal-Sindhudurg2.webp",
 		],
 	},
 	{
@@ -97,16 +98,16 @@ const architecturalProjects = [
 		title: "Project for Mr. Sarjoshi",
 		location: "Achara, Malvan",
 		type: "architectural",
-		imageLink: "https://i.ibb.co/QJH7kqX/Mr-Sarjoshi-Achara-Malvan1.webp",
-		otherImages: ["https://i.ibb.co/Z6PypnH/Mr-Sarjoshi-Achara-Malvan2.webp"],
+		imageLink: "https://i.ibb.co/d2Mxs9P/Mr-Sarjoshi-Achara-Malvan1.webp",
+		otherImages: ["https://i.ibb.co/CBGRk4V/Mr-Sarjoshi-Achara-Malvan2.webp"],
 	},
 	{
 		id: "08",
 		title: "Project for Mr. Vikas Mane",
 		location: "Islampur",
 		type: "architectural",
-		imageLink: "https://i.ibb.co/zH2XpN8/Mr-Vikas-Mane-Islampur1.webp",
-		otherImages: ["https://i.ibb.co/pdC1DJr/Mr-Vikas-Mane-Islampur2.webp"],
+		imageLink: "https://i.ibb.co/Mcq3rn2/Mr-Vikas-Mane-Islampur1.webp",
+		otherImages: ["https://i.ibb.co/SmyGrnK/Mr-Vikas-Mane-Islampur2.webp"],
 	},
 	{
 		id: "09",
@@ -114,9 +115,9 @@ const architecturalProjects = [
 		location: "Patgaon",
 		type: "architectural",
 		imageLink:
-			"https://i.ibb.co/42QBSFP/Shree-Mouni-Maharaj-Math-Patgaon1.webp",
+			"https://i.ibb.co/3vZy2sz/Shree-Mouni-Maharaj-Math-Patgaon1.webp",
 		otherImages: [
-			"https://i.ibb.co/m8ft40R/Shree-Mouni-Maharaj-Math-Patgaon2.webp",
+			"https://i.ibb.co/c3WHgr4/Shree-Mouni-Maharaj-Math-Patgaon2.webp",
 		],
 	},
 	{
@@ -124,11 +125,11 @@ const architecturalProjects = [
 		title: "Project for Mr. Rohan Kadam",
 		location: "-",
 		type: "interior",
-		imageLink: "https://i.ibb.co/GHg6Vmd/Mr-Rohan-Kadam-1-jpeg.webp",
+		imageLink: "https://i.ibb.co/TLWwLmv/Mr-Rohan-Kadam-1-jpeg.webp",
 		otherImages: [
-			"https://i.ibb.co/DfWrXg0/Mr-Rohan-Kadam-2-jpeg.webp",
-			"https://i.ibb.co/hLF4LNX/Mr-Rohan-Kadam-3-jpeg.webp",
-			"https://i.ibb.co/DzcPCfV/Mr-Rohan-Kadam-4.webp",
+			"https://i.ibb.co/26fSx6s/Mr-Rohan-Kadam-2-jpeg.webp",
+			"https://i.ibb.co/4dQqvsC/Mr-Rohan-Kadam-3-jpeg.webp",
+			"https://i.ibb.co/kh6jc7n/Mr-Rohan-Kadam-4.webp",
 		],
 	},
 	{
@@ -136,7 +137,7 @@ const architecturalProjects = [
 		title: "Project for Mr. Chetan Desai",
 		location: "-",
 		type: "interior",
-		imageLink: "https://i.ibb.co/nnjvtJV/Mr-Chetan-Desai.webp",
+		imageLink: "https://i.ibb.co/vhbDDfp/Mr-Chetan-Desai.webp",
 		otherImages: [],
 	},
 	{
@@ -144,9 +145,12 @@ const architecturalProjects = [
 		title: "Project for Ray Associates",
 		location: "Kolhapur",
 		type: "interior",
-		imageLink: "https://i.ibb.co/KzGdxTQ/Ray-Associates-Kop.webp",
+		imageLink: "https://i.ibb.co/cD1H4zT/Ray-Associates-Kop.webp",
 		otherImages: [],
 	},
 ];
+
+
+
 
 export { photos, projectImages, architecturalProjects };
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,80 +1,73 @@
 import React, { useState } from "react";
 import { architecturalProjects } from "../Data";
 import { useNavigate } from "react-router-dom";
-import { MdClear } from "react-icons/md";
 
 function Projects() {
-
 	const [filter, setFilter] = useState([]);
 
 	const handleFilterChange = (type) => {
-	  if (filter.includes(type)) {
-		setFilter(filter.filter((t) => t !== type));
-	  } else {
-		setFilter([...filter, type]);
-	  }
+		if (filter.includes(type)) {
+			setFilter(filter.filter((t) => t !== type));
+		} else {
+			setFilter([...filter, type]);
+		}
 	};
-  
+
 	const filteredItems = filter.length
-	  ? architecturalProjects.filter((project) => filter.includes(project.type))
-	  : architecturalProjects;
-	
+		? architecturalProjects.filter((project) => filter.includes(project.type))
+		: architecturalProjects;
+
 	return (
 		<>
 			<div className="mt-[7rem] flex justify-center w-[25%] mx-auto">
 				<div
 					className={`w-[50%] py-2 text-center border border-gray-600 rounded-s-full cursor-pointer ${
-						filter.includes('architectural') ? "focus: bg-[#777B7E] focus: text-yellow-300" : ""
+						filter.includes("architectural")
+							? "focus: bg-[#777B7E] focus: text-yellow-300"
+							: ""
 					}`}
-					onClick={() => handleFilterChange('architectural')}>
+					onClick={() => handleFilterChange("architectural")}>
 					Architectural
 				</div>
 				<div
 					className={`w-[50%] py-2 text-center border border-gray-600 rounded-e-full cursor-pointer ${
-						filter.includes('interior') ? "focus: bg-[#777B7E] focus: text-yellow-300" : ""
+						filter.includes("interior")
+							? "focus: bg-[#777B7E] focus: text-yellow-300"
+							: ""
 					}`}
-					onClick={() => handleFilterChange('interior')}>
+					onClick={() => handleFilterChange("interior")}>
 					Interior
 				</div>
 
-				{/* Clear button logic
-				{(archFilter || interiorFilter) && (
-					<div>
-						<button>
-							<MdClear />
-						</button>
-					</div>
-				)} */}
-
-				{/* architectural or interior filter logic */}
+			
 			</div>
 			<div className="flex w-[90%] m-auto  mt-28 flex-col justify-center items-center md:grid-cols-2 md:grid  gap-8">
 				{filteredItems.map((project) => (
-						<div
-							key={project.id}
-							className=" m-auto flex flex-col"
-							onClick={() =>
-								navigate(`/projects/${project.id}/${project.title}`)
-							}>
-							<div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-xl bg-clip-border rounded-xl w-96">
-								<div class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-									<img
-										className="object-fit absolute scale-[1.30]"
-										src={project.imageLink}
-										alt="card-image"
-									/>
-								</div>
-								<div class="p-6">
-									<h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-										{project.title}
-									</h5>
-									<p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-										{project.location}
-									</p>
-								</div>
+					<div
+						key={project.id}
+						className=" m-auto flex flex-col"
+						onClick={() =>
+							navigate(`/projects/${project.id}/${project.title}`)
+						}>
+						<div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-xl bg-clip-border rounded-xl w-96">
+							<div class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+								<img
+									className="object-fit absolute scale-[1.30]"
+									src={project.imageLink}
+									alt="card-image"
+								/>
+							</div>
+							<div class="p-6">
+								<h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+									{project.title}
+								</h5>
+								<p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+									{project.location}
+								</p>
 							</div>
 						</div>
-					))}
+					</div>
+				))}
 			</div>
 		</>
 	);
